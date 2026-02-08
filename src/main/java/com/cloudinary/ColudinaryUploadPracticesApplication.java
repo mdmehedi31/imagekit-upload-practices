10c518fc765f51d868e43d2fb10134d1c9a1c2fb
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-02-08 20:42:10 +0600

    setup for upload

---
+++ src/main/java/com/cloudinary/ColudinaryUploadPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.cloudinary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColudinaryUploadPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColudinaryUploadPracticesApplication.class, args);
+    }
+
+}
